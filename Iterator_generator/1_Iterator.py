a57613f44fea452fcdc9c05a1a562d134a6b4af7
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-05-01 21:49:54 +0800

    简洁的介绍了一下Iterator 和Iterable 的区别，相比上次添加了一句有助于理解的话

--- Iterator_generator/1_Iterator.py
+++ Iterator_generator/1_Iterator.py
@@ -8,10 +8,12 @@ lst = [1,2,3]
 for i in lst:           # for 循环后的lst 必须是一个iterable 可迭代对象
     print(i)
 
+# 简洁的介绍一下Iterator 和Iterable 的区别
 # Iterator 和Iterable 很相似但并不一样
 # Iterator 是一个表示数据流的对象，可以使用next()方法不断从这个对象中获取数据
 # 对于一个Iterable 来说，它可以没有状态，可以根据一个Iterable 产生一个Iterator。
 # 而Iterator 必须有状态，即当前迭代到的位置，而且它没有用于修改内部数据的接口
 # 从实现上来看，一个Iterable 一定要有__iter__() 方法，或者__getitem__() 方法，这是为了保证这个Iterable 可以被iter() 函数转换成Iterator。
 # 而Iterator 必须要有__next__() 方法，使其可以被next() 函数调用，迭代出数据。
+# 所以，到这里可以发现，Iterable 就是Iterator-able，一个能够能够变成Iterator 的对象。
 # 而for 循环可以理解成，在循环开始前，创建了一个Iterable 对应的Iterator
