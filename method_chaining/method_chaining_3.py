bb2747c3f8c26d709c801117860ffa2efa35ece3
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-05-19 21:32:51 +0800

    返回值也可以不是自身，而是一个副本

---
+++ method_chaining/method_chaining_3.py
@@ -0,0 +1,25 @@
+import numpy as np
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class Vector:
+    x: float
+    y: float
+    z: float
+
+    def normalized(self):
+        x, y, z = self.x, self.y, self.z
+        norm = np.sqrt(x * x + y * y + z * z)
+        return type(self)(x / norm, y / norm, z / norm)
+
+    def reflected(self):
+        return type(self)(-self.x, -self.y, -self.z)
+
+def main():
+    p = Vector(1., 2., 3.)
+    q = p.reflected().normalized()
+    print(p)
+    print(q)
+
+if __name__ == "__main__":
+    main()
